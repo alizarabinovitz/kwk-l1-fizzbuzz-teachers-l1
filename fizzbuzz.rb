1e0cd4b3e7a34bc3fc68b6116df6040a7be17cc1
Aliza Rabinovitz <alizajessa24@gmail.com>
2018-06-19 16:56:37 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,14 @@
 def fizzbuzz(int)
-  if int%15 == 0
-    return "FizzBuzz"
+  if int%3 == 0 && int%5 == 0
+     "FizzBuzz"
 
   elsif int%3 == 0
-    return "Fizz"
+    "Fizz"
 
   elsif int%5 == 0
-    return "Buzz"
+    "Buzz"
 
   else
-    return "nil"
+    "nil"
   end
 end
